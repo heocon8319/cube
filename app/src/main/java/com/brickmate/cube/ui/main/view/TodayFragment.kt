5c68ebdf004015baab4e571f65ca198357441e2a
vihuynh <vihuynh@brickmate.kr>
2020-11-04 17:28:36 +0700

    Custom layout row calendar

--- app/src/main/java/com/brickmate/cube/ui/main/view/TodayFragment.kt
+++ app/src/main/java/com/brickmate/cube/ui/main/view/TodayFragment.kt
@@ -1,18 +1,18 @@
 package com.brickmate.cube.ui.main.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import com.brickmate.cube.R
 import com.brickmate.cube.ui.base.BaseFragment
-import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
-import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
-import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
-import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
-import com.michalsvec.singlerowcalendar.utils.DateUtils
+import com.brickmate.cube.ui.custom.singlerowcalendar.calendar.CalendarChangesObserver
+import com.brickmate.cube.ui.custom.singlerowcalendar.calendar.CalendarViewManager
+import com.brickmate.cube.ui.custom.singlerowcalendar.calendar.SingleRowCalendarAdapter
+import com.brickmate.cube.ui.custom.singlerowcalendar.selection.CalendarSelectionManager
+import com.brickmate.cube.ui.custom.singlerowcalendar.utils.DateUtils
 import kotlinx.android.synthetic.main.fragment_today.*
 import kotlinx.android.synthetic.main.layout_item_row_calendar.view.*
 import java.util.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,7 +31,6 @@ class TodayFragment : BaseFragment() {
 
     private val calendar = Calendar.getInstance()
     private var currentMonth = 0
-//    private val rowCalendar = crRowCalendar
 
     override fun layoutId() = R.layout.fragment_today
 
@@ -55,6 +54,7 @@ class TodayFragment : BaseFragment() {
         // Set current date, month
         calendar.time = Date()
         currentMonth = calendar[Calendar.MONTH]
+        tvMonth.text = currentMonth.toString()
 
         // Layout
         val rowCalendarViewManager = object :
@@ -78,12 +78,12 @@ class TodayFragment : BaseFragment() {
                 // Set date
                 val cal = Calendar.getInstance()
                 cal.time = date
-
                 // Return layout when selected
                 return if (isSelected)
                     R.layout.layout_item_row_calendar_selected
                 else
                     R.layout.layout_item_row_calendar
+
             }
         }
 
@@ -91,38 +91,12 @@ class TodayFragment : BaseFragment() {
             CalendarChangesObserver {
             override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
                 tvMonth.text = DateUtils.getMonthNumber(date)
-
-                val currentDay = DateUtils.getDayNumber(date).toInt()
-
-
-                val cal = Calendar.getInstance()
-                val endMonthDay = cal.getActualMaximum(Calendar.DATE)
-                if (endMonthDay <= (currentDay - 2))
-                    currentMonth++
                 super.whenSelectionChanged(isSelected, position, date)
-            }
-
-            override fun whenCalendarScrolled(dx: Int, dy: Int) {
-                Log.d("viht", "dx : " + dx + " dy : " + dy)
-                super.whenCalendarScrolled(dx, dy)
-            }
-
-            override fun whenSelectionRestored() {
-                Log.d("viht", "restore")
-                super.whenSelectionRestored()
-            }
-
-            override fun whenSelectionRefreshed() {
-                Log.d("viht", "refresh")
-                super.whenSelectionRefreshed()
             }
         }
 
         val rowCalendarSelectionManager = object : CalendarSelectionManager {
             override fun canBeItemSelected(position: Int, date: Date): Boolean {
-                // Set date
-                val cal = Calendar.getInstance()
-                cal.time = date
                 return true
             }
         }
@@ -131,58 +105,12 @@ class TodayFragment : BaseFragment() {
             calendarViewManager = rowCalendarViewManager
             calendarChangesObserver = rowCalendarChangesObserver
             calendarSelectionManager = rowCalendarSelectionManager
+            futureDaysCount = 1000
+            pastDaysCount = 500
             includeCurrentDate = true
-            setDates(getFutureDatesOfCurrentMonth())
             init()
         }
     }
-
-    private fun goToNextMonth() {
-        //rowCalendar.setDates(getDatesOfNextMonth())
-    }
-
-    private fun getDatesOfNextMonth(): List<Date> {
-        currentMonth++ // + because we want next month
-        if (currentMonth == 12) {
-            // we will switch to january of next year, when we reach last month of year
-            calendar.set(Calendar.YEAR, calendar[Calendar.YEAR] + 1)
-            currentMonth = 0 // 0 == january
-        }
-        return getDates(mutableListOf())
-    }
-
-    private fun getDatesOfPreviousMonth(): List<Date> {
-        currentMonth-- // - because we want previous month
-        if (currentMonth == -1) {
-            // we will switch to december of previous year, when we reach first month of year
-            calendar.set(Calendar.YEAR, calendar[Calendar.YEAR] - 1)
-            currentMonth = 11 // 11 == december
-        }
-        return getDates(mutableListOf())
-    }
-
-    private fun getFutureDatesOfCurrentMonth(): List<Date> {
-        // get all next dates of current month
-        currentMonth = calendar[Calendar.MONTH]
-        return getDates(mutableListOf())
-    }
-
-
-    private fun getDates(list: MutableList<Date>): List<Date> {
-        // load dates of whole month
-//        calendar.set(Calendar.MONTH, currentMonth)
-        calendar.set(Calendar.DAY_OF_MONTH, 1)
-        list.add(calendar.time)
-        while (currentMonth == calendar[Calendar.MONTH]) {
-            calendar.add(Calendar.DATE, +1)
-            list.add(calendar.time)
-//            if (calendar[Calendar.MONTH] == currentMonth)
-//                list.add(calendar.time)
-        }
-        calendar.add(Calendar.DATE, -1)
-        return list
-    }
-
 
     companion object {
         /**
