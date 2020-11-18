87045590ef943ba2a018b2ac4fd89dcd0b8e5da3
vihuynh <vihuynh@brickmate.kr>
2020-11-18 12:01:01 +0700

    Custom layout measure history dialog

--- app/src/main/java/com/brickmate/cube/ui/main/adapter/TodayMeasureHistoryAdapter.kt
+++ app/src/main/java/com/brickmate/cube/ui/main/adapter/TodayMeasureHistoryAdapter.kt
@@ -1,10 +1,20 @@
 package com.brickmate.cube.ui.main.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.brickmate.cube.R
+import com.google.android.material.tabs.TabLayout
 
-class TodayMeasureHistoryAdapter(fm: FragmentManager, behavior: Int) : FragmentPagerAdapter(fm, behavior) {
+
+class TodayMeasureHistoryAdapter(fm: FragmentManager, behavior: Int, private val mContext: Context) : FragmentPagerAdapter(fm, behavior) {
 
     var mFragmentCollection: MutableList<Fragment> = ArrayList()
     var mTitleCollection: MutableList<String> = ArrayList()
@@ -25,5 +35,41 @@ class TodayMeasureHistoryAdapter(fm: FragmentManager, behavior: Int) : FragmentP
     //Needed for
     override fun getPageTitle(position: Int): CharSequence? {
         return mTitleCollection[position]
+    }
+
+
+    fun getTabView(position: Int): View? {
+        val tabTitles = intArrayOf(
+            R.string.dialog_measure_history_text_daily,
+            R.string.dialog_measure_history_text_weekly,
+            R.string.dialog_measure_history_text_monthly
+        )
+        val image = intArrayOf(R.drawable.bg_tab1_white, R.drawable.bg_tab2_white, R.drawable.bg_tab3_white)
+        val v: View = LayoutInflater.from(mContext).inflate(R.layout.layout_custom_tab, null)
+        val tvTitle = v.findViewById<View>(R.id.tvTitle) as TextView
+        tvTitle.text = mContext.getString(tabTitles[position])
+        val clBackground = v.findViewById<View>(R.id.clBackground) as ConstraintLayout
+        clBackground.setBackgroundResource(image[position])
+        return v
+    }
+
+    fun onSelectedView(tabLayout: TabLayout, position: Int) {
+        val image = intArrayOf(R.drawable.bg_tab1_green, R.drawable.bg_tab2_green, R.drawable.bg_tab3_green)
+        val tab = tabLayout.getTabAt(position)
+        val selected: View? = tab!!.customView
+        val tvTitle = selected?.findViewById<View>(R.id.tvTitle) as TextView
+        tvTitle.setTextColor(mContext.resources.getColor(R.color.white))
+        val clBackground = selected?.findViewById<View>(R.id.clBackground) as ConstraintLayout
+        clBackground.setBackgroundResource(image[position])
+    }
+
+    fun onUnselectedView(tabLayout: TabLayout, position: Int) {
+        val image = intArrayOf(R.drawable.bg_tab1_white, R.drawable.bg_tab2_white, R.drawable.bg_tab3_white)
+        val tab = tabLayout.getTabAt(position)
+        val selected: View? = tab!!.customView
+        val tvTitle = selected?.findViewById<View>(R.id.tvTitle) as TextView
+        tvTitle.setTextColor(mContext.resources.getColor(R.color.rolling_stone))
+        val clBackground = selected?.findViewById<View>(R.id.clBackground) as ConstraintLayout
+        clBackground.setBackgroundResource(image[position])
     }
 }
--- app/src/main/java/com/brickmate/cube/ui/main/view/dialog/TodayMeasureHistoryDialog.kt
+++ app/src/main/java/com/brickmate/cube/ui/main/view/dialog/TodayMeasureHistoryDialog.kt
@@ -12,7 +12,10 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.brickmate.cube.R
 import com.brickmate.cube.ui.main.adapter.TodayMeasureHistoryAdapter
 import com.brickmate.cube.ui.main.view.TodayMeasureHistoryFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import kotlinx.android.synthetic.main.dialog_today_measure_history.*
+
 
 class TodayMeasureHistoryDialog : DialogFragment() {
 
@@ -22,22 +25,50 @@ class TodayMeasureHistoryDialog : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = TodayMeasureHistoryAdapter(childFragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
-        adapter.addFragment("Boy", TodayMeasureHistoryFragment.newInstance(0))
-        adapter.addFragment("Girl", TodayMeasureHistoryFragment.newInstance(1))
-        adapter.addFragment("Robot", TodayMeasureHistoryFragment.newInstance(2))
+
+        tvTitle.text = resources.getString(R.string.dialog_measure_history_text_growth_record)
+
+        val adapter =
+            TodayMeasureHistoryAdapter(childFragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, this.requireContext())
+        adapter.addFragment("", TodayMeasureHistoryFragment.newInstance(0))
+        adapter.addFragment("", TodayMeasureHistoryFragment.newInstance(1))
+        adapter.addFragment("", TodayMeasureHistoryFragment.newInstance(2))
+
         vpGraph.adapter = adapter
         tlGraph.setupWithViewPager(vpGraph)
-        tvTitle.text = resources.getString(R.string.dialog_measure_history_text_growth_record)
+
+        for (i in 0 until tlGraph.tabCount) {
+            val tab: TabLayout.Tab? = tlGraph.getTabAt(i)
+            tab?.customView = adapter.getTabView(i)
+        }
+        adapter.onSelectedView(tlGraph, 0)
+
+        initListener(adapter)
     }
 
     override fun onResume() {
         super.onResume()
-//        val params: ViewGroup.LayoutParams = dialog!!.window!!.attributes
-//        params.width = ViewGroup.LayoutParams.MATCH_PARENT
-//        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
-//        dialog!!.window!!.attributes = params as WindowManager.LayoutParams
+        val params: ViewGroup.LayoutParams = dialog!!.window!!.attributes
+        params.width = ViewGroup.LayoutParams.WRAP_CONTENT
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog!!.window!!.attributes = params as WindowManager.LayoutParams
         dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+    }
+
+    private fun initListener(adapter: TodayMeasureHistoryAdapter) {
+        tlGraph.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val customTab = tab.position
+                adapter.onSelectedView(tlGraph, customTab)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                val customTab = tab.position
+                adapter.onUnselectedView(tlGraph, customTab)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
     }
 
     companion object {
